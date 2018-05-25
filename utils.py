a2f76608629df2616373ac6ec66edb5a55f7722f
qlew <lars.erik.wuerflinger@gmail.com>
2018-05-25 15:13:45 +0200

    fix docstring

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@
 
 
 def name_estimators(estimators):
-    """Return names and estimators"""
+    """Return names and estimators."""
     names = [type(estimator).__name__.lower() for estimator in estimators]
     return zip(names, estimators)
